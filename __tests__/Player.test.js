7e87d88997994ea667718e15f29635373352e056
Matt Gross <mbgross111@gmail.com>
2021-02-15 18:50:22 -0700

    add tested Player constructor

---
+++ __tests__/Player.test.js
@@ -0,0 +1,20 @@
+const { expect } = require('@jest/globals');
+const Player = require('../lib/Player.js');
+
+const Potion = require('../lib/potion');
+
+jest.mock('../lib/Potion.js');
+
+console.log(new Potion());
+
+test('creates a player object', () => {
+  const player = new Player('Dave');
+
+  expect(player.name).toBe('Dave');
+  expect(player.health).toEqual(expect.any(Number));
+  expect(player.strength).toEqual(expect.any(Number));
+  expect(player.agility).toEqual(expect.any(Number));
+  expect(player.inventory).toEqual(
+    expect.arrayContaining([expect.any(Object)])
+  );
+});
